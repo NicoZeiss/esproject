854e9e8efbf635e05092f6bcea78dcafd3e9f63e
NicoZeiss <zeissern@gmail.com>
2024-05-23 03:08:56 +0200

    Configure static files handling in Django
    The configuration for static files in Django has been modified. In the settings.py file, the STATIC_URL was updated, and in the urls.py file, urlpatterns were adjusted to serve static files during development. This ensures the correct serving of static files in different environments.

--- config/urls.py
+++ config/urls.py
@@ -1,4 +1,6 @@
+from django.conf import settings
 from django.contrib import admin
+from django.conf.urls.static import static
 from django.urls import path, include
 
 
@@ -6,3 +8,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('patients_management.urls')),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
